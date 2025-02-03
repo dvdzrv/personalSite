a6760f9f53045223ca6a00c8555c0af184e41737
Dávid Szomolai <davidszomolai1@gmail.com>
2025-02-03 12:52:11 +0100

    Init

---
+++ index.php
@@ -0,0 +1,165 @@
+<!doctype html>
+<html lang="sk">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Portfolio</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <h1>Dávid Szomolai:</h1>
+        <h1>Osobné portfólio</h1>
+        <img src="" alt="Logo">
+        <br>
+        <nav>
+            <a href="">LINK1</a>
+            <a href="">LINK2</a>
+            <a href="">LINK3</a>
+        </nav>
+    </header>
+
+
+
+    <main>
+        <section>
+            <h3>Názov projektu</h3>
+            <article>
+                <img src="http://192.168.100.106/images/lily.webp" alt="Projekt image">
+
+
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium aliquid autem consequatur delectus eos esse eum excepturi hic inventore iste iusto odit omnis praesentium quas quo, sit veniam, vero vitae.
+                <br>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, error, minima. Aliquam deserunt dignissimos dolor eaque, eum fugiat id itaque, magnam natus non officiis provident quisquam, sapiente sed voluptas voluptatem. <br>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus inventore iste modi qui voluptate? Aliquam cum ea ipsa magni natus non possimus! Consequuntur enim est ex praesentium sunt tempora veniam.
+                <br>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur deleniti doloremque eligendi est fuga, laborum nemo nesciunt nisi obcaecati, porro quam qui quibusdam quidem quos recusandae reiciendis rem velit veniam.</p>
+                <br>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur deleniti doloremque eligendi est fuga, laborum nemo nesciunt nisi obcaecati, porro quam qui quibusdam quidem quos recusandae reiciendis rem velit veniam.</p>
+                <br>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur deleniti doloremque eligendi est fuga, laborum nemo nesciunt nisi obcaecati, porro quam qui quibusdam quidem quos recusandae reiciendis rem velit veniam.</p>
+                <br>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur deleniti doloremque eligendi est fuga, laborum nemo nesciunt nisi obcaecati, porro quam qui quibusdam quidem quos recusandae reiciendis rem velit veniam.</p>
+                <br>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur deleniti doloremque eligendi est fuga, laborum nemo nesciunt nisi obcaecati, porro quam qui quibusdam quidem quos recusandae reiciendis rem velit veniam.</p>
+                <br>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur deleniti doloremque eligendi est fuga, laborum nemo nesciunt nisi obcaecati, porro quam qui quibusdam quidem quos recusandae reiciendis rem velit veniam.</p>
+                <br>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur deleniti doloremque eligendi est fuga, laborum nemo nesciunt nisi obcaecati, porro quam qui quibusdam quidem quos recusandae reiciendis rem velit veniam.</p>
+                <br>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur deleniti doloremque eligendi est fuga, laborum nemo nesciunt nisi obcaecati, porro quam qui quibusdam quidem quos recusandae reiciendis rem velit veniam.</p>
+                <br>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur deleniti doloremque eligendi est fuga, laborum nemo nesciunt nisi obcaecati, porro quam qui quibusdam quidem quos recusandae reiciendis rem velit veniam.</p>
+                <br>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur deleniti doloremque eligendi est fuga, laborum nemo nesciunt nisi obcaecati, porro quam qui quibusdam quidem quos recusandae reiciendis rem velit veniam.</p>
+                <br>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur deleniti doloremque eligendi est fuga, laborum nemo nesciunt nisi obcaecati, porro quam qui quibusdam quidem quos recusandae reiciendis rem velit veniam.</p>
+                <br>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur deleniti doloremque eligendi est fuga, laborum nemo nesciunt nisi obcaecati, porro quam qui quibusdam quidem quos recusandae reiciendis rem velit veniam.</p>
+                <br>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur deleniti doloremque eligendi est fuga, laborum nemo nesciunt nisi obcaecati, porro quam qui quibusdam quidem quos recusandae reiciendis rem velit veniam.</p>
+                <br>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur deleniti doloremque eligendi est fuga, laborum nemo nesciunt nisi obcaecati, porro quam qui quibusdam quidem quos recusandae reiciendis rem velit veniam.</p>
+                <br>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur deleniti doloremque eligendi est fuga, laborum nemo nesciunt nisi obcaecati, porro quam qui quibusdam quidem quos recusandae reiciendis rem velit veniam.</p>
+                <br>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur deleniti doloremque eligendi est fuga, laborum nemo nesciunt nisi obcaecati, porro quam qui quibusdam quidem quos recusandae reiciendis rem velit veniam.</p>
+                <br>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur deleniti doloremque eligendi est fuga, laborum nemo nesciunt nisi obcaecati, porro quam qui quibusdam quidem quos recusandae reiciendis rem velit veniam.</p>
+                <br>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur deleniti doloremque eligendi est fuga, laborum nemo nesciunt nisi obcaecati, porro quam qui quibusdam quidem quos recusandae reiciendis rem velit veniam.</p>
+                <br>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur deleniti doloremque eligendi est fuga, laborum nemo nesciunt nisi obcaecati, porro quam qui quibusdam quidem quos recusandae reiciendis rem velit veniam.</p>
+                <br>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur deleniti doloremque eligendi est fuga, laborum nemo nesciunt nisi obcaecati, porro quam qui quibusdam quidem quos recusandae reiciendis rem velit veniam.</p>
+                <br>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur deleniti doloremque eligendi est fuga, laborum nemo nesciunt nisi obcaecati, porro quam qui quibusdam quidem quos recusandae reiciendis rem velit veniam.</p>
+                <br>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur deleniti doloremque eligendi est fuga, laborum nemo nesciunt nisi obcaecati, porro quam qui quibusdam quidem quos recusandae reiciendis rem velit veniam.</p>
+                <br>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur deleniti doloremque eligendi est fuga, laborum nemo nesciunt nisi obcaecati, porro quam qui quibusdam quidem quos recusandae reiciendis rem velit veniam.</p>
+                <br>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur deleniti doloremque eligendi est fuga, laborum nemo nesciunt nisi obcaecati, porro quam qui quibusdam quidem quos recusandae reiciendis rem velit veniam.</p>
+                <br>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur deleniti doloremque eligendi est fuga, laborum nemo nesciunt nisi obcaecati, porro quam qui quibusdam quidem quos recusandae reiciendis rem velit veniam.</p>
+                <br>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur deleniti doloremque eligendi est fuga, laborum nemo nesciunt nisi obcaecati, porro quam qui quibusdam quidem quos recusandae reiciendis rem velit veniam.</p>
+                <br>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur deleniti doloremque eligendi est fuga, laborum nemo nesciunt nisi obcaecati, porro quam qui quibusdam quidem quos recusandae reiciendis rem velit veniam.</p>
+                <br>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur deleniti doloremque eligendi est fuga, laborum nemo nesciunt nisi obcaecati, porro quam qui quibusdam quidem quos recusandae reiciendis rem velit veniam.</p>
+                <br>
+            </article>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum earum eius tempora temporibus. Aliquam aut beatae earum fuga labore obcaecati reprehenderit, tempore. Alias amet facere id iste neque sunt veniam.</p>
+        </section>
+    </main>
+
+
+
+    <aside class="float_left">
+        <h2>Lokálny chat:</h2>
+        <p>Čauko, zanechaj svoj unikátny odkaz na tejto stránke:</p>
+        <form action="">
+            <label for="meno">Tvoje ctené meno:</label>
+            <input type="text" name="meno" id="meno">
+            <label for="sprava">Tvoja jedinečná správa:</label>
+            <input type="text" name="sprava" id="sprava">
+            <button type="submit">Poslať</button>
+        </form>
+        <section style="overflow:scroll;" class="chats" id="chatBar">
+            <p class="chats"><b>Jožo:</b> Super stránka bro.</p>
+            <?php
+            $DB_host = "172.30.0.101";
+            $DB_user = "root";
+            $DB_pass = "Sklad123.";
+            $DB_name = "SzomolaiProjekt";
+
+            $DB_con = new mysqli($DB_host, $DB_user, $DB_pass, $DB_name);
+
+            $DB_req = "SELECT name, message FROM massages WHERE 1";
+            $DB_res = $DB_con -> query($DB_req);
+
+            while($row = $DB_res -> fetch_assoc()){
+                echo "<p><b>".$row['name']."</b>: ".$row['message']."</p>";
+            }
+
+            $DB_res->close();
+            ?>
+        </section>
+
+    </aside>
+
+
+
+    <aside class="float_right">
+        <h1>RIGHT ASIDE</h1>
+        <p>
+            Vitaj na mojej osobnej stránke, užívateľ.
+        </p>
+        <p>
+            Cieľom tejto stránky je sa čo najautenticky priblížiť vizuálu Web 1.0 a zárovaň obsahovať moderné funkcie.
+        </p>
+        <p>
+            Zdrojový kód tejto stránky bude publikovaný na <a href="https://github.com/dvdzrv/">mojom Githube.</a> Prosím inšpiruj sa alebo mi pomôž a moju stránku oprav.
+        </p>
+    </aside>
+
+
+    <footer>
+        <p>
+            Kontakt: <br>
+            Email: email@email.com <br>
+            Adresse: Adresse <br>
+        </p>
+        <p>
+            Links: <br> <br>
+            <a href="https://myanimelist.net/animelist/Davidzroznavy"><img class="imgLink" src="./images/MyAnimeList_Logo.png" alt="MyAnimeList Logo"></a>
+            <a href="https://www.instagram.com/davidzroznavy/"><img class="imgLink" src="./images/instagram.png" alt="Instagram Logo"></a>
+            <a href="https://www.tiktok.com/@davidzroznavy"><img class="imgLink" src="./images/tiktok.png" alt="TikTok Logo"></a>
+            <a href="https://github.com/dvdzrv/"><img class="imgLink" src="./images/github.webp" alt="GitHub Logo"></a>
+        </p>
+    </footer>
+</body>
+</html>
