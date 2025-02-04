b6d34c2f49af6ea2aa7195af6b772cf3b4cb940b
Dávid Szomolai <davidszomolai1@gmail.com>
2025-02-04 20:22:35 +0100

    Pridanie loga

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
     <header>
         <h1>Dávid Szomolai:</h1>
         <h1>Osobné portfólio</h1>
-        <img src="images/logo.png" alt="Logo">
+        <img src="images/logo.png" alt="Logo" style="height: auto; width: 1%; display: inline; margin-top: 0.1em;">
         <br>
         <nav>
             <a href="">LINK1</a>
