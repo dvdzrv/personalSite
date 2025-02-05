4d08229f8ac6b709814109891413c19e856609ae
Dávid Szomolai <davidszomolai1@gmail.com>
2025-02-05 08:18:09 +0100

    oprava labelov

--- index.php
+++ index.php
@@ -103,9 +103,9 @@
         <h2>Zanechaj komentár:</h2>
         <p>Čauko, zanechaj svoj unikátny odkaz na tejto stránke:</p>
         <form action="uloz.php" method="post">
-            <label for="meno">Tvoje ctené meno:</label>
+            <label for="name">Tvoje ctené meno:</label>
             <input type="text" name="name" id="name">
-            <label for="sprava">Tvoja jedinečná správa:</label>
+            <label for="message">Tvoja jedinečná správa:</label>
             <input type="text" name="message" id="message">
             <button type="submit">Poslať</button>
             <button class="refreshIMG"><a class="refreshIMG" href="index.php">Refresh</a></button>
