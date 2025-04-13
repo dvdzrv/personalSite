abb06bc9e11dc40f75055d8bfe491b22f60fca7f
Dávid Szomolai <davidszomolai1@gmail.com>
2025-04-13 23:49:00 +0200

    Pridanie článku

--- index.php
+++ index.php
@@ -159,7 +159,7 @@
     <aside class="float_right">
         <h1>Uvítanie</h1>
         <p>
-            Vitaj na mojej osobnej stránke, užívateľ. Moje meno je Dávid Szomolai, tiež nazývaný Kaneki alebo dvdzrv (davidzroznavy).
+            Vitaj, na mojej osobnej stránke, užívateľ. Moje meno je Dávid Szomolai, tiež nazývaný Kaneki alebo dvdzrv (davidzroznavy).
         </p>
         <p>
             Cieľom tejto stránky je sa čo najautenticky priblížiť vizuálu blogu v ére Web 1.0 a zárovaň obsahovať moderné funkcie.
@@ -173,9 +173,9 @@
         <br>
         <br>
         <br>
-        <a href="login.php"><img style="width: 35%; height: auto;" src="images/xenia1.png" alt="Xenia The Linux Fox"></a>
-        <img src="images/valid-html-blue.png" alt="This site has valid HTML!">
-        <img src="images/valid-css-blue.png" alt="This site has valid CSS!">
+        <a href="login.php"><img style="width: 35%; height: auto;" src="images/xenia1.png" alt="Xenia The Linux Fox" class="small_tags"></a>
+        <img src="images/valid-html-blue.png" alt="This site has valid HTML!" class="small_tags">
+        <img src="images/valid-css-blue.png" alt="This site has valid CSS!" class="small_tags">
     </aside>
 
 
